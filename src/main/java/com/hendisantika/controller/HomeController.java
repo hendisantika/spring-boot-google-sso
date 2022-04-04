ca7a50360d9c39bb2ae4ff714ebed0a6e3baf12a
Hendi Santika <hendisantika@gmail.com>
2022-04-04 07:11:01 +0700

    #20 - Add HomeController

---
+++ src/main/java/com/hendisantika/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.hendisantika.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-google-sso
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/04/22
+ * Time: 07.10
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+}
